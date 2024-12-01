4d87f8e5ab35621fbef322d88eaf5de3738e773f
Tumen <tuvanchap_tumen@mail.ru>
2024-12-01 12:25:54 -0500

    new_start

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 cp ./monitor_test.sh /usr/local/bin/
-cp ./monitor_test.service /usr/lib/systemd/monitor_test.service
-cp ./monitor_test.timer /usr/lib/systemd/monitor_test.timer
+cp ./monitor_test.service /usr/lib/systemd/system/
+cp ./monitor_test.timer /usr/lib/systemd/system/
 
 systemctl daemon-reload
+systemctl enable monitor_test.service
+
 systemctl enable monitor_test.timer
 systemctl start monitor_test.timer
 systemctl status monitor_test.timer
